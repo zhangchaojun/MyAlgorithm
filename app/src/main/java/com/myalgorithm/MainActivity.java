651cae50133dd3a82aecaf272533d9fda0eb3aa0
zhangchaojun <565978459@qq.com>
2019-03-20 15:31:51 +0800

    加选择排序

--- app/src/main/java/com/myalgorithm/MainActivity.java
+++ app/src/main/java/com/myalgorithm/MainActivity.java
@@ -17,16 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bubbleSort(nums);
+        bubbleSort2(nums);
         Log.e(TAG, "onCreate: " + Arrays.toString(nums));
     }
 
+    //********************************冒泡排序 start************************************//
+
     /**
-     * 冒泡排序
-     *
-     * @param nums
+     * 是否需要进行下一轮比较，初始化为true。作为外层循环的与&&条件
+     * 每次外层循环开始的时候就置为false
+     * 就是每次外层循环开始的时候都奔着不用再来一次去的，减少比较次数
+     * 如果新的一轮循环根本就没有进入（前一个数比后一个数大的条件里），说明内层循环了一遍前一个数都比后一个数小
+     * 所以就不会置为true，那么就不会进入下一次的外层循环了，等于是提前跳出循环。
+     * 不会死板的每次都执行满（n-1）次
      */
-    public void bubbleSort(int[] nums) {
+
+    private void bubbleSort1(int[] numbers) {
+        boolean flag = true;
+        for (int i = 0; i < numbers.length - 1 && flag; i++) {
+            flag = false;
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int sum = numbers[j] + numbers[j + 1];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = sum - numbers[j];
+                    flag = true;//还需要下一次外层循环
+                }
+            }
+        }
+    }
+
+    public void bubbleSort2(int[] nums) {
         int temp = 0;
         int size = nums.length;
         boolean flag = true;
@@ -44,4 +65,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    //********************************冒泡排序  end************************************//
+
+
+    //********************************选择排序 start************************************//
+
+    private void selectSort(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int k = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[k]) {
+                    k = j;
+                }
+            }
+            if (k != i) {
+                int temp = numbers[k];
+                numbers[k] = numbers[i];
+                numbers[i] = temp;
+            }
+
+        }
+    }
+
+    //********************************选择排序  end************************************//
+
 }
